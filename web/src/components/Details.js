66e1c81cc4ed3d66453ec6dfc8930515565980d9
Virginia Álvarez <virginiaalvarez.ar@gmail.com>
2023-02-09 11:55:48 +0100

    add skeleton of node project

---
+++ web/src/components/Details.js
@@ -0,0 +1,14 @@
+const Details = () =>{
+    return(
+        <section>
+            <p>Gabinete</p>
+            <p>Hora</p>
+            <p>Descripción</p>
+            <p>Observaciones</p>
+        </section>
+
+
+    )
+}
+
+export default Details;
